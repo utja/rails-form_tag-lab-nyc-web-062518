9fc6fae4492be3e63888fd369176880b540e9ec2
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-07-22 18:00:02 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new #shows the new form
+  end
+
+  def create #posts the new form to create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
   end
 
 end
